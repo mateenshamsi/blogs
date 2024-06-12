8b133e8859921fcbb89365eb4e690b2461169805
mateenshamsi <matinshamsi16@gmail.com>
2024-06-12 22:36:13 +0530

    done

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://blogs-m0zq524eu-mateenshamsis-projects.vercel.app',
         changeOrigin: true,
         secure: false,
       },
